f8d45ee096755f2a66a84e1264da3c2f837cbd14
Kachel Wittig <kjwittig@gmail.com>
2018-04-23 17:36:53 +0000

    automatically backed up by learn

--- fixtures/kickstarter_scraper.rb
+++ fixtures/kickstarter_scraper.rb
@@ -4,6 +4,7 @@ require "nokogiri"
   def create_project_hash
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
+    binding.pry
   end
 
 create_project_hash
